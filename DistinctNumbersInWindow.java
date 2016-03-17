58398e1492517311d168fbf5729c1c2864ae12b7
Anton Smolich <a.smolich@gmail.com>
2016-03-16 22:39:13 -0700

    DistinctNumbersInWindow

---
+++ DistinctNumbersInWindow.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class DistinctNumbersInWindow {
+    public int[] distinctNums(int[] a, int b) {
+        if (a == null || b < 1) return new int[0];
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(b);
+        int i = 0;
+        int bound = Math.min(b, a.length);
+        for (; i < bound; i++) {
+            int ai = a[i];
+            Integer count = map.get(ai);
+            if (count == null) count = 0;
+            map.put(ai, count + 1);
+        }
+
+        int[] res = new int[a.length - b + 1];
+        res[0] = map.keySet().size();
+        for (int j = 1; i < a.length; i++) {
+            int aj1 = a[j - 1];
+            int ai = a[i];
+            Integer count = map.get(aj1);
+            if (count == null) count = 0;
+            if (count <= 1)
+                map.remove(aj1);
+            else 
+                map.put(aj1, count - 1);
+
+            count = map.get(ai);
+            if (count == null) count = 0;
+            map.put(ai, count + 1);
+
+            res[j] = map.keySet().size();
+    
+            j++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        DistinctNumbersInWindow dist = new DistinctNumbersInWindow();
+        System.out.println(Arrays.toString(dist.distinctNums(new int[]{1, 2, 1, 3, 4, 3}, 3)));
+    }
+}

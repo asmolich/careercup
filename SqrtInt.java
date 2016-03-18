b607e4c8c16af4f3a1b2d771bf223673ff1c3e45
Anton Smolich <a.smolich@gmail.com>
2016-03-18 15:23:23 -0700

    fixed overflow issue

--- SqrtInt.java
+++ SqrtInt.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class SqrtInt {
     public static void main(String[] args) {
         SqrtInt sqrt = new SqrtInt();
-        System.out.println(sqrt.sqrt(120));
+        System.out.println(sqrt.sqrt(930675566));
     }
 
     public int sqrt(int a) {
@@ -10,23 +10,25 @@ public class SqrtInt {
 
         int lo = 1;
         int hi = a - 1;
-        int closest = a;
-        int res = 0;
+        long closest = a;
+        int res = -1;
         while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            long sq = mid * mid;
+            int mid = lo + ((hi - lo) >> 1);
+
+            long sq = (long)mid * mid;
             if (sq == a) return mid;
             else {
-                long diff = Math.abs(sq - a);
-                if (closest > diff) {
-                    closest = (int)diff;
-                    res = mid;
-                }
                 if (sq > a) {
                     hi = mid - 1;
                 }
                 else {
                     lo = mid + 1;
+
+                    long diff = a - sq;
+                    if (closest > diff) {
+                        closest = diff;
+                        res = mid;
+                    } 
                 }
             }
         }

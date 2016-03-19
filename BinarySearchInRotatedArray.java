df8e928abc282d616b32f0aa956d70ffc219d1bc
Anton Smolich <a.smolich@gmail.com>
2016-03-19 09:52:39 -0700

    binary search in rotated array

--- BinarySearchInRotatedArray.java
+++ BinarySearchInRotatedArray.java
@@ -5,16 +5,17 @@ public class BinarySearchInRotatedArray {
         //System.out.println(bs.search(Arrays.asList(4, 5, 6, 7, 0, 1, 2), 4));
         //System.out.println(bs.search(Arrays.asList(101, 103, 106, 109, 158, 164, 182, 187, 202, 205, 2, 3, 32, 57, 69, 74, 81, 99, 100), 202));
         System.out.println(bs.search(Arrays.asList(3, 4, 5, 6, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), 4));
+        //System.out.println(bs.search(Arrays.asList(1, 7, 67, 133, 178), 1));
     }
     public int search(final List<Integer> a, int b) {
         if (a == null || a.isEmpty()) return -1;
         
-        //System.out.println(a);
+        System.out.println(a);
         int n = a.size();
         int pivot = searchPivot(a);
         int lo = pivot;
         int hi = pivot + n - 1;
-        //System.out.println("pivot = " + pivot);
+        System.out.println("pivot = " + pivot);
 
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
@@ -52,14 +53,13 @@ public class BinarySearchInRotatedArray {
             }
 
             if (mid < a.size() - 1 && a.get(mid) > a.get(mid + 1)) return mid + 1;
-            else if (mid > 0 && a.get(mid) > a.get(mid - 1)) return mid;
+            else if (mid > 0 && a.get(mid) < a.get(mid - 1)) return mid;
             else if (a.get(mid) > a.get(hi)) {
                 lo = mid + 1;
             }
             else {
                 hi = mid - 1;
             }
-            //3 3 3 3 3 3 3 3 3 4 5 6 1 2 3
         }
         return 0;
     }

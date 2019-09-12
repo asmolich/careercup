4061d8896d2c313f294ab3339f79cbb0e63b0a11
Anton Smolich <anton.smolich@hbo.com>
2019-09-12 09:08:24 -0700

    leetcode problem 864. Shortest Path to Get All Keys

---
+++ ShortestPathToGetAllKeys.java
@@ -0,0 +1,103 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * LeetCode
+ * 864. Shortest Path to Get All Keys
+ * https://leetcode.com/problems/shortest-path-to-get-all-keys/
+ */
+public class ShortestPathToGetAllKeys {
+
+    public static void main(String[] args) {
+        System.out.println(shortestPathAllKeys(new String[]{
+                "@.a.#",
+                "###.#",
+                "b.A.B"
+        }));
+        System.out.println(shortestPathAllKeys(new String[]{
+                "@..aA",
+                "..B#.",
+                "....b"
+        }));
+        System.out.println(shortestPathAllKeys(new String[]{
+                "@...a",
+                ".###A",
+                "b.BCc"
+        }));
+        System.out.println(shortestPathAllKeys(new String[]{
+                ".#.b.",
+                "A.#aB",
+                "#d...",
+                "@.cC.",
+                "D...#"
+        }));
+    }
+
+    private enum Key {
+        a, b, c, d, e, f
+    }
+
+    private static class Node {
+        int x, y, steps;
+        int keys;
+
+        static Node of(int x, int y, int steps, int keys) {
+            Node node = new Node();
+            node.x = x;
+            node.y = y;
+            node.steps = steps;
+            node.keys = keys;
+            return node;
+        }
+    }
+
+    private static int shortestPathAllKeys(String[] grid) {
+        if (grid == null) return -1;
+
+        int n = grid.length;
+        int m = grid[0].length();
+
+        int allKeys = 0;
+        Queue<Node> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                char ch = grid[i].charAt(j);
+                if (ch == '@') {
+                    queue.add(Node.of(i, j, 0, 0));
+                } else if (ch >= 'a' && ch <= 'f') {
+                    allKeys |= (1 << Key.valueOf(Character.toString(ch)).ordinal());
+                }
+            }
+        }
+
+        boolean[][][] visited = new boolean[n][m][allKeys];
+        int[][] dims = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node.keys == allKeys) return node.steps;
+
+            if (visited[node.x][node.y][node.keys]) continue;
+            visited[node.x][node.y][node.keys] = true;
+
+            for (int[] dim : dims) {
+                int x = node.x + dim[0];
+                int y = node.y + dim[1];
+                int keys = node.keys;
+                if (x < 0 || x >= n || y < 0 || y >= m) continue;
+
+                char c = grid[x].charAt(y);
+                if (c == '#') continue;
+                if (c >= 'A' && c <= 'F' && hasKey(keys, c)) continue;
+                if (c >= 'a' && c <= 'f') {
+                    keys |= (1 << Key.valueOf(Character.toString(c)).ordinal());
+                }
+                queue.add(Node.of(x, y, node.steps + 1, keys));
+            }
+        }
+        return -1;
+    }
+
+    private static boolean hasKey(int keys, char c) {
+        return (keys & (1 << Key.valueOf(Character.toString(Character.toLowerCase(c))).ordinal())) == 0;
+    }
+}
--- Subsets.java
+++ Subsets.java
@@ -1,42 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
-public class Subsets {
-
+/**
+ * PowerSet. LeetCode.
+ * 78. Subsets https://leetcode.com/problems/subsets/
+ */
+public class PowerSet {
     public static void main(String[] args) {
-//        System.out.println(subsets(Arrays.asList(1, 2, 3, 4)));
-        System.out.println(subsets(Arrays.asList(15, 20, 12, 19, 4)));
+
     }
 
-    private static List<List<Integer>> subsets(List<Integer> set) {
-        List<List<Integer>> result = new ArrayList<>();
 
-        List<Integer> subset = new ArrayList<>();
-        result.add(subset);
-
-        if (set == null || set.isEmpty()) return result;
-
-        set.sort(Comparator.comparingInt(a -> a));
-
-        for (Integer i : set) {
-            new ArrayList<>(result).forEach(r -> {
-                ArrayList<Integer> newSubset = new ArrayList<>(r);
-                newSubset.add(i);
-                result.add(newSubset);
-            });
-        }
-
-        result.sort((a, b) -> {
-            int n = Math.min(a.size(), b.size());
-            for (int i = 0; i < n; i++) {
-                final int compare = Integer.compare(a.get(i), b.get(i));
-                if (compare != 0) return compare;
-            }
-            return Integer.compare(a.size(), b.size());
-        });
-
-        return result;
-    }
 }

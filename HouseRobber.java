ffe564154973d875d7fbe9eea434ce727258738b
Anton Smolich <anton.smolich@hbo.com>
2019-09-16 20:14:51 -0700

    house robber

---
+++ HouseRobber.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/**
+ * LeetCode. 198. House Robber
+ * https://leetcode.com/problems/house-robber/
+ */
+public class HouseRobber {
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+        System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
+    }
+
+    private static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int n = nums.length;
+        int[] value = new int[n + 2];
+        for (int i = 2; i < n + 2; i++) {
+            value[i] = Math.max(value[i - 1], value[i - 2] + nums[i - 2]);
+        }
+        return value[n + 1];
+    }
+}
